67a336f1bc7a08671db7b1c587cb49858d6cc7c0
Souvick-Mondal <mondalsouvick07@gmail.com>
2020-08-29 12:12:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -53,8 +53,4 @@ cacheSolve <- function(x, ...) {
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
